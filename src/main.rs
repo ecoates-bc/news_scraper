4157b6d499e223e6425e0b8e501c95d5bc47e04b
Edith Coates <ecoates.bc@gmail.com>
2023-04-03 20:00:55 -0700

    new tokenizer and encoder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::path::*;
 
+use crate::preprocess::{tokenize_headline_text, load_tokenizer};
+
 mod prepare_dataset;
 mod preprocess;
 mod scrape_data;
@@ -8,13 +10,16 @@ fn main() {
     let root_dir = Path::new("scraped");
     let raw_data =
         prepare_dataset::get_raw_dataset(root_dir, 0.2).expect("Dataset could not be loaded.");
-    let tokenizer =
-        preprocess::load_tokenizer("bert-base-cased").expect("Tokenizer could not be loaded");
+    let tokenizer = load_tokenizer().expect("Could not load tokenizer.");
+    dbg!("Tokenizer loaded...");
 
     let indexer = preprocess::create_word_indexer(&tokenizer, &raw_data.train)
         .expect("Cannot construct indexer.");
     let deindexer = preprocess::create_word_deindexer(&indexer);
 
-    dbg!(indexer["into"]);
-    dbg!(&deindexer[&indexer["into"]]);
+    let entry = raw_data.train.last().unwrap();
+    let tokens = tokenize_headline_text(&tokenizer, &entry.path).unwrap();
+    dbg!(&tokens);
+    dbg!(indexer.encode_sequence(&tokens));
+    dbg!(deindexer.decode_sequence(&indexer.encode_sequence(&tokens)));
 }
--- src/preprocess.rs
+++ src/preprocess.rs
@@ -3,12 +3,61 @@ use std::collections::hash_map::RandomState;
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
 use std::{fs, io};
-use tokenizers::tokenizer::{Result, Tokenizer};
+use nlprule::Tokenizer;
 
 use crate::prepare_dataset::ArticleEntry;
 
-pub fn load_tokenizer(identifier: &str) -> Result<Tokenizer> {
-    Tokenizer::from_pretrained(identifier, None)
+pub struct Indexer {
+    index_map: HashMap<String, usize>,
+}
+
+impl Indexer {
+    fn new(index_map: HashMap<String, usize>) -> Indexer {
+        Indexer {
+            index_map: index_map,
+        }
+    }
+
+    pub fn get_index(&self, token: &String) -> usize {
+        if self.index_map.contains_key(token) {
+            self.index_map[token]
+        } else {
+            self.index_map.len() + 1
+        }
+    }
+
+    pub fn encode_sequence(&self, tokens: &Vec<String>) -> Vec<usize> {
+        tokens.into_iter().map(|tok| self.get_index(tok)).collect()
+    }
+}
+
+pub struct Deindexer {
+    token_map: HashMap<usize, String>,
+}
+
+impl Deindexer {
+    fn new(token_map: HashMap<usize, String>) -> Deindexer {
+        Deindexer {
+            token_map: token_map,
+        }
+    }
+
+    pub fn get_token(&self, index: usize) -> String {
+        if index > self.token_map.len() {
+            "UNK".into()
+        } else {
+            self.token_map[&index].clone()
+        }
+    }
+
+    pub fn decode_sequence(&self, idx: &Vec<usize>) -> Vec<String> {
+        idx.into_iter().map(|id| self.get_token(*id)).collect()
+    }
+}
+
+pub fn load_tokenizer() -> anyhow::Result<Tokenizer> {
+    dbg!("Loading tokenizer...");
+    Ok(Tokenizer::new("./en_tokenizer.bin")?)
 }
 
 pub fn read_article_text(path: &Path) -> io::Result<String> {
@@ -20,20 +69,37 @@ pub fn read_article_text(path: &Path) -> io::Result<String> {
     }
 }
 
-pub fn tokenize_article_text(tokenizer: &Tokenizer, article_path: &Path) -> Result<Vec<String>> {
+fn get_tokens(tokenizer: &Tokenizer, text: &String) -> Vec<String> {
+    let tokens = tokenizer.pipe(text).flat_map(|s| s.tokens().to_vec());
+    let tokens = tokens.map(|t| t.word().as_str().to_string());
+    tokens.collect()
+}
+
+pub fn tokenize_article_text(tokenizer: &Tokenizer, article_path: &Path) -> anyhow::Result<Vec<String>> {
     let article_text = read_article_text(article_path)?;
-    let encoding = tokenizer.encode(article_text, false)?;
-    Ok(encoding.get_tokens().to_vec())
+    Ok(get_tokens(tokenizer, &article_text))
+}
+
+pub fn tokenize_headline_text(tokenizer: &Tokenizer, article_path: &Path) -> anyhow::Result<Vec<String>> {
+    let headline = article_path
+        .file_stem()
+        .ok_or(anyhow::anyhow!("Could not find stem."))?;
+    let headline = headline
+        .to_str()
+        .ok_or(anyhow::anyhow!("Could not convert to str"))?;
+    let headline = String::from(headline).replace("_", " ");
+    dbg!(get_tokens(tokenizer, &headline));
+    Ok(get_tokens(tokenizer, &headline))
 }
 
 pub fn create_word_indexer(
     tokenizer: &Tokenizer,
     ds: &Vec<ArticleEntry>,
-) -> anyhow::Result<HashMap<String, usize>> {
+) -> anyhow::Result<Indexer> {
     let article_tokens = ds
         .into_iter()
         .map(
-            |entry| match tokenize_article_text(tokenizer, &entry.path) {
+            |entry| match tokenize_headline_text(tokenizer, &entry.path) {
                 Ok(tokens) => tokens,
                 Err(_) => vec![],
             },
@@ -42,13 +108,18 @@ pub fn create_word_indexer(
     let token_set: HashSet<String, RandomState> = HashSet::from_iter(article_tokens);
     let indexer = HashMap::from_iter(token_set.iter().enumerate().map(|(i, s)| (s.clone(), i)));
     if indexer.len() > 0 {
-        Ok(indexer)
+        Ok(Indexer::new(indexer))
     } else {
         Err(anyhow::anyhow!("Could not create indexer."))
     }
 }
 
-pub fn create_word_deindexer(indexer: &HashMap<String, usize>) -> HashMap<usize, String> {
-    let deidx_iter = indexer.into_iter().map(|(s, i)| (*i, s.clone()));
-    HashMap::from_iter(deidx_iter)
+pub fn create_word_deindexer(indexer: &Indexer) -> Deindexer {
+    let deidx_iter = indexer
+        .index_map
+        .clone()
+        .into_iter()
+        .map(|(s, i)| (i, s.clone()));
+    let deidxer = HashMap::from_iter(deidx_iter);
+    Deindexer::new(deidxer)
 }
